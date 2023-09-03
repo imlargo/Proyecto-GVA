08e16e30ec28dc7387f3bbb9781a9584502c43a1
Juan Carlos Largo <jlargob@unal.edu.co>
2023-09-03 15:29:15 -0500

    Update Clase #1 - Vectores y el espacio.py

--- Unidad #1 - Los fundamentos/Clase #1 - Vectores y el espacio.py
+++ Unidad #1 - Los fundamentos/Clase #1 - Vectores y el espacio.py
@@ -33,75 +33,32 @@ class PlaneScene(LinearTransformationScene):
             )
         
     def construct(self):
-        self.vectores()
+        self.sum_vectores([1,2], [-2, -1])
         self.wait()
         
         
     def vectores(self):
-        vector1 = Vector([1, 2], color = PURPLE_A)
-        label1 = vector1.coordinate_label(color = PURPLE_A)
-        
-        vector2 = Vector([2, -1], color = TEAL_A)
-        label2 = vector2.coordinate_label(color = TEAL_A)
-        
-        vector3 = Vector([3, 1], color = YELLOW_C)
-        label3 = vector3.coordinate_label(color = YELLOW_C)
-        
-        titulo1 = Tex("Suma de vectores :3").to_corner(UP + LEFT)
-        titulo2 = Tex("El orden no importa!").to_corner(UP + LEFT)
-        
-        newVector1 = Vector([1, 2], color = PURPLE_A)
-        
-        sumaVec = (MathTex(r"\vec{A} + \vec{B} = \vec{X}")).to_corner(DOWN + LEFT)
-        sumaComp = MathTex(r"\begin{bmatrix} 1\\ 2\\ \end{bmatrix} + \begin{bmatrix} 2\\ -1\\ \end{bmatrix}").to_corner(DOWN + LEFT)
-        sumaResult = MathTex(r"\begin{bmatrix} 1\\ 2\\ \end{bmatrix} + \begin{bmatrix} 2\\ -1\\ \end{bmatrix} = \begin{bmatrix} 3\\ 1\\ \end{bmatrix}").to_corner(DOWN + LEFT)
-        
-        self.play(Create(vector1), Create(label1))
-        self.play(Create(vector2), Create(label2))
-        
-        self.play(Create(titulo1))
-        
-        self.play(Write(sumaVec))
-        self.wait(1)
-        self.play(Transform(sumaVec, sumaComp))
-        self.wait(1)
-        self.play(ApplyMethod(vector1.shift, vector2.get_end()))
-        self.play(Create(vector3), Create(label3), Transform(sumaComp, sumaResult))
-        
-        #Segunda parte ---
-        self.wait(1)
-        self.play(Transform(titulo1, titulo2), FadeOut(vector3), FadeOut(label3))
-        self.wait()
-    
-        self.play(Transform(vector1, newVector1))
-        self.wait(1)
-        self.play(ApplyMethod(vector2.shift, newVector1.get_end()))
-        
-        self.wait()
-        self.play(Create(vector3), Create(label3))
-        self.wait(2)
-
         pass
         
         
-    def sum_vectores(self):
-        vector1 = Vector([1, 2], color = PURPLE_A)
-        label1 = vector1.coordinate_label(color = PURPLE_A)
+    def sum_vectores(self, v1, v2):
+        vector1 = Vector(v1, color = PURPLE_A)
+        label1 = vector1.coordinate_label(color = PURPLE_A).scale(0.7)
         
-        vector2 = Vector([2, -1], color = TEAL_A)
-        label2 = vector2.coordinate_label(color = TEAL_A)
+        vector2 = Vector(v2, color = TEAL_A)
+        label2 = vector2.coordinate_label(color = TEAL_A).scale(0.7)
         
-        vector3 = Vector([3, 1], color = YELLOW_C)
-        label3 = vector3.coordinate_label(color = YELLOW_C)
+        vector3 = Vector((np.array(v1) + np.array(v2)), color = YELLOW_C)
+        label3 = vector3.coordinate_label(color = YELLOW_C).scale(0.7)
         
         titulo1 = Tex("Suma de vectores :3").to_corner(UP + LEFT)
         titulo2 = Tex("El orden no importa!").to_corner(UP + LEFT)
         
-        newVector1 = Vector([1, 2], color = PURPLE_A)
+        newVector1 = Vector(np.array(v1), color = PURPLE_A)
         
-        sumaVec = (MathTex(r"\vec{A} + \vec{B} = \vec{X}")).to_corner(DOWN + LEFT)
-        sumaComp = MathTex(r"\begin{bmatrix} 1\\ 2\\ \end{bmatrix} + \begin{bmatrix} 2\\ -1\\ \end{bmatrix}").to_corner(DOWN + LEFT)
-        sumaResult = MathTex(r"\begin{bmatrix} 1\\ 2\\ \end{bmatrix} + \begin{bmatrix} 2\\ -1\\ \end{bmatrix} = \begin{bmatrix} 3\\ 1\\ \end{bmatrix}").to_corner(DOWN + LEFT)
+        sumaVec = (MathTex(r"\vec{A} + \vec{B} = \vec{X}")).to_corner(DOWN + LEFT).scale(0.7)
+        sumaComp = MathTex(r"\begin{bmatrix} 1\\ 2\\ \end{bmatrix} + \begin{bmatrix} 2\\ -1\\ \end{bmatrix}").to_corner(DOWN + LEFT).scale(0.7)
+        sumaResult = MathTex(r"\begin{bmatrix} 1\\ 2\\ \end{bmatrix} + \begin{bmatrix} 2\\ -1\\ \end{bmatrix} = \begin{bmatrix} 3\\ 1\\ \end{bmatrix}").to_corner(DOWN + LEFT).scale(0.7)
         
         self.play(Create(vector1), Create(label1))
         self.play(Create(vector2), Create(label2))
@@ -131,17 +88,32 @@ class PlaneScene(LinearTransformationScene):
         
         pass
     
-    def resta_vectores(self):
+    def resta_vectores(self):    
+        pass
+    
+    
+    
+    
+    def rotar_vectores(self, vector, angulo):
         
-
-        
+        vector1 = Vector(vector, color = PURPLE_A)
+        label1 = vector1.coordinate_label(color = PURPLE_A)
+            
+        vector2 = Vector(rot_vec(vector, angulo), color = TEAL_A)
+        label2 = vector2.coordinate_label(color = TEAL_A)
+        self.play(Create(vector1), Create(label1))
+        self.wait()
+        self.play(Rotate(vector1, angle= np.radians(angulo), about_point=ORIGIN, rate_func=linear))
+        self.wait()
+        self.play(Transform(vector1, vector2), Transform(label1, label2))
+        self.wait()
         pass
         
         
 test = PlaneScene()
 test.render()
 from IPython.display import clear_output
-#clear_output()
+clear_output()
 from IPython.display import HTML
 from base64 import b64encode
 video_path =  '/content/media/videos/1080p60/PlaneScene.mp4'
