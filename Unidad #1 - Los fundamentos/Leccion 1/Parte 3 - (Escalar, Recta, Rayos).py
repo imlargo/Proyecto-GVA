403fe3db992e5db358bfa11e6e86ae7e8cdd075a
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2023-09-12 05:55:30 -0500

    Update Parte 3 - (Escalar, Recta, Rayos).py

--- Unidad #1 - Los fundamentos/Leccion 1/Parte 3 - (Escalar, Recta, Rayos).py
+++ Unidad #1 - Los fundamentos/Leccion 1/Parte 3 - (Escalar, Recta, Rayos).py
@@ -23,7 +23,9 @@ class PlaneScene(LinearTransformationScene):
     def construct(self):
         self.escalando()
         self.wait()
-    
+
+    def resaltar(self, objeto):
+        pass    
 
     def escalando(self):
         
@@ -32,34 +34,39 @@ class PlaneScene(LinearTransformationScene):
         #Hacemos la pareja ordenada v = (2,1) como vector
         vector = Vector(v, color = RED_A)
         name = MathTex(r"n \cdot \vec{v}", color = RED_A)
-        label = vector.coordinate_label(color = RED_A)
+        label = vector.coordinate_label(color = RED_A).to_corner(UP+LEFT)
+
         
         #Vector aleatorio?
-        n = ValueTracker(0)
+        n = ValueTracker(1)
         
         graph = ImplicitFunction(lambda x, y: y - ((1/2) * x),color = PURPLE_A).set_stroke(opacity = 0.7, width=2) 
         
         punto = always_redraw(lambda: Dot(point = np.array(escalar(v,n.get_value()) + [0]), color = YELLOW))
         vector = always_redraw(lambda: Vector(escalar(v,n.get_value()), color = RED_A))
-        name = always_redraw(lambda: MathTex(r"n \cdot \vec{v}", color = RED_A).shift((vector.get_end() - vector.get_start()) / 2).shift(UP * 0.5))
+        name = always_redraw(lambda: MathTex(str(round(n.get_value(),1)) + r" \cdot \vec{v}", color = RED_A).shift((vector.get_end() - vector.get_start()) / 2).shift(UP * 0.5))
+        vlabel = always_redraw(lambda: MathTex(str(round(n.get_value(),1)) + r" \cdot \begin{bmatrix} 2\\ 1\end{bmatrix}", color = RED_A).to_corner(UP+LEFT))
 
-        self.play(Create(vector), Create(punto), Create(name), Create(punto))
+        nameBefore = MathTex(r"n \cdot \vec{v}", color = RED_A).shift((vector.get_end() - vector.get_start()) / 2).shift(UP * 0.5)
+        labelBefore = MathTex(r"n \cdot \begin{bmatrix} 2\\ 1\end{bmatrix}", color = RED_A).to_corner(UP+LEFT)
+
+        self.play(Create(vector), Create(punto), Create(nameBefore), Create(labelBefore))
+        self.wait(1)
+        self.play(Create(name), Create(vlabel), FadeOut(labelBefore), FadeOut(nameBefore))
         
         #Cambiando los valores de n
-        self.play(n.animate.set_value(1), run_time = 3)
-        self.wait(1)  
         self.play(n.animate.set_value(2), run_time = 3)
-        self.wait(0.5)
+        self.wait(1)
         self.play(n.animate.set_value(-1), Create(graph), run_time = 2)
-        self.wait()
+        self.wait(0.5)
         self.play(n.animate.set_value(-3), run_time = 3)
         self.play(n.animate.set_value(2.5), run_time = 2)
 
-        self.play(FadeOut(vector), FadeOut(punto), FadeOut(name))
-
+        self.play(FadeOut(vector), FadeOut(punto), FadeOut(name), FadeOut(vlabel))
         pass
 
     def rayo_recta(self):
+
         pass
     
     def dependencia_lineal(self):
