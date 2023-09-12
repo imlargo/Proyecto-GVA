26ec6e9fc34708aaecd1d58b59478186ae170d8b
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2023-09-12 07:59:39 -0500

    Update Parte 3 - (Escalar, Recta, Rayos).py

--- Unidad #1 - Los fundamentos/Leccion 1/Parte 3 - (Escalar, Recta, Rayos).py
+++ Unidad #1 - Los fundamentos/Leccion 1/Parte 3 - (Escalar, Recta, Rayos).py
@@ -33,16 +33,15 @@ class PlaneScene(LinearTransformationScene):
     def escalando(self):
         
         v = [2,1]
-       
+        n = ValueTracker(1)
         #Hacemos la pareja ordenada v = (2,1) como vector
         vector = Vector(v, color = RED_A)
         name = MathTex(r"n \cdot \vec{v}", color = RED_A)
         label = vector.coordinate_label(color = RED_A).to_corner(UP+LEFT)
 
         
-        #Vector aleatorio?
-        n = ValueTracker(1)
-        
+        # --- Escalamos el vector con una animacion continua ---
+        #Definimos todas las variables
         graph = ImplicitFunction(lambda x, y: y - ((1/2) * x),color = PURPLE_A).set_stroke(opacity = 0.7, width=2) 
         
         punto = always_redraw(lambda: Dot(point = np.array(escalar(v,n.get_value()) + [0]), color = YELLOW))
@@ -53,8 +52,9 @@ class PlaneScene(LinearTransformationScene):
         nameBefore = MathTex(r"n \cdot \vec{v}", color = RED_A).shift((vector.get_end() - vector.get_start()) / 2).shift(UP * 0.5)
         labelBefore = MathTex(r"n \cdot \begin{bmatrix} 2\\ 1\end{bmatrix}", color = RED_A).to_corner(UP+LEFT)
 
-        rectaGen = MathTex(r"L_{\vec{v}}").move_to(np.array([-3, 2, 0])).scale(1.25)
+        rectaGen = MathTex(r"L_{\vec{v}}\\ n \cdot \vec{v} \ | \ n \in \mathbb{R}").move_to(np.array([-3, 2, 0])).scale(1.25)
         
+        #Creamos el vector
         self.play(Create(vector), Create(punto), Create(nameBefore), Create(labelBefore))
         self.wait(1)
         self.play(FadeOut(labelBefore), FadeOut(nameBefore), Create(name), Create(vlabel))
@@ -67,6 +67,7 @@ class PlaneScene(LinearTransformationScene):
         self.play(n.animate.set_value(-3), run_time = 3)
         self.play(n.animate.set_value(2.5), run_time = 2)
 
+        #Resaltamos los datos importantes
         self.play(FadeOut(vector), FadeOut(punto), FadeOut(name), FadeOut(vlabel))
         self.play((ShowPassingFlash(
                 ImplicitFunction(lambda x, y: y - ((1/2) * x),color = BLUE),
@@ -82,18 +83,14 @@ class PlaneScene(LinearTransformationScene):
 
     def rayo_recta(self):
         v = [2,1]
-       
+        n = ValueTracker(1)       
         #Hacemos la pareja ordenada v = (2,1) como vector
         vector = Vector(v, color = RED_A)
         name = MathTex(r"n \cdot \vec{v}", color = RED_A)
         label = vector.coordinate_label(color = RED_A).to_corner(UP+LEFT)
 
-        
-        #Vector aleatorio?
-        n = ValueTracker(1)
-        
-        graph = ImplicitFunction(lambda x, y: y - ((1/2) * x),color = PURPLE_A).set_stroke(opacity = 0.7, width=2) 
-        
+        # --- Escalamos el vector con una animacion continua ---
+        #Definimos todas las variables
         punto = always_redraw(lambda: Dot(point = np.array(escalar(v,n.get_value()) + [0]), color = YELLOW))
         vector = always_redraw(lambda: Vector(escalar(v,n.get_value()), color = RED_A))
         name = always_redraw(lambda: MathTex(str(round(n.get_value(),1)) + r" \cdot \vec{v}", color = RED_A).shift((vector.get_end() - vector.get_start()) / 2).shift(UP * 0.5))
@@ -105,43 +102,41 @@ class PlaneScene(LinearTransformationScene):
         nameBefore = MathTex(r"n \cdot \vec{v}", color = RED_A).shift((vector.get_end() - vector.get_start()) / 2).shift(UP * 0.5)
         labelBefore = MathTex(r"n \cdot \begin{bmatrix} 2\\ 1\end{bmatrix}", color = RED_A).to_corner(UP+LEFT)
 
-        rectaGen = MathTex(r"L_{\vec{v}}").move_to(np.array([-3, 2, 0])).scale(1.25)
-        rayGen = MathTex(r"R_{\vec{v}}^{+}").move_to(np.array([5, 1, 0])).scale(1.25)
-        raynegGen = MathTex(r"R_{\vec{v}}^{-}").move_to(np.array([-2, -2.5, 0])).scale(1.25)
+        rayGen = MathTex(r"R_{\vec{v}}^{+}\\ n \cdot \vec{v} \ | \ n > 0").move_to(np.array([5, 1, 0])).scale(1.25)
+        raynegGen = MathTex(r"R_{\vec{v}}^{-}\\ n \cdot \vec{v} \ | \ n < 0 ").move_to(np.array([-2, -2.5, 0])).scale(1.25)
 
+        #Creamos los objetos en el plano
         self.play(Create(vector), Create(punto), Create(nameBefore), Create(labelBefore))
         self.wait(1)
         self.play(FadeOut(labelBefore), FadeOut(nameBefore), Create(name), Create(vlabel))
 
-        #Cambiando los valores de n
+        #... Rayo Positivo ...
         self.play(n.animate.set_value(2), Create(rayPos), Create(rayGen), run_time = 3)
         self.resaltar(rayGen, 1, TEAL_C)
         self.play(n.animate.set_value(0.5), run_time = 2)
         self.play(n.animate.set_value(0.25), run_time = 2)
         self.play(n.animate.set_value(3), run_time = 2)
         self.play(n.animate.set_value(1), run_time = 2)
-
+        #Resaltamos los datos importantes
         self.play(
             (ShowPassingFlash((rayPos.copy().set_color(TEAL_C)), run_time=2, time_width=0.2)),
             (ShowPassingFlash(SurroundingRectangle(rayGen, color = TEAL_C, buff = 0.25), run_time=2, time_width=0.2))
             )
         self.play(FadeOut(rayPos), FadeOut(rayGen)) 
         
-        #Rayo negativo
+        #... Rayo negativo ...
         self.play(n.animate.set_value(-1), Create(rayNeg), Create(raynegGen), run_time = 3)
         self.resaltar(raynegGen, 1, TEAL_C)
         self.play(n.animate.set_value(-0.5), run_time = 2)
         self.play(n.animate.set_value(-0.25), run_time = 2)
         self.play(n.animate.set_value(-3), run_time = 2)
         self.play(n.animate.set_value(-1), run_time = 2)
-
+        #Resaltamos los datos importantes
         self.play(
             (ShowPassingFlash((rayNeg.copy().set_color(TEAL_C)), run_time=2, time_width=0.2)),
             (ShowPassingFlash(SurroundingRectangle(raynegGen, color = TEAL_C, buff = 0.25), run_time=2, time_width=0.2))
             )
         self.play(FadeOut(rayNeg), FadeOut(raynegGen))
-        
-
         pass
     
     def dependencia_lineal(self):
