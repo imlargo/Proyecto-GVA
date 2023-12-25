6648c230c58b264af836288f1501adf18fc936f1
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2023-12-25 20:34:17 +0000

    work: web page

--- src/index.js
+++ src/index.js
@@ -51,16 +51,3 @@ function concierto() {
     document.getElementById("concierto").textContent = `Faltan ${dias} días, ${horas} horas y ${minutos} minutos hasta el 23 de febrero de 2024 a las 8:00 PM.`
 }
 concierto();
-
-function changeMode(button) {
-    const body = document.body;
-    const isDark = body.classList.contains('dark');
-
-    if (isDark) {
-        body.classList.remove('dark');
-        button.classList = "bi bi-moon theme-button";
-    } else {
-        body.classList.add('dark');
-        button.classList = "bi bi-brightness-low theme-button";
-    }
-}
