452de5b4eeda2e54a333b9515869a28114c6da5c
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2024-01-09 15:43:17 -0500

    _

--- animations/homepage.py
+++ animations/homepage.py
@@ -5,7 +5,6 @@ import numpy as np;
 
 class MainScene(Scene):
     def construct(self):
-        self.camera.background_color = getColor("#16265C")
         # > - Crear grid del plano - <
         planeGrid = NumberPlane(
             background_line_style = {
@@ -32,3 +31,19 @@ class MainScene(Scene):
 
         ]
         # > - Animacion onda vectores - <
+
+
+    def vectorSpace():
+        initFunc = lambda pos: np.sin(pos[1]) * RIGHT + np.cos(pos[0]) * UP
+        funcs = [
+            VectorField.scale_func(initFunc, 0.5),
+            VectorField.scale_func(initFunc, 0.2)
+        ]
+
+        vector_field = ArrowVectorField(initFunc)
+        self.add(vector_field)
+        self.wait()
+
+        for func in funcs:
+            self.play(vector_field.animate.become(ArrowVectorField(func)))
+            self.wait()
