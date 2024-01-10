565be0d8644add70abdef6a56683f5c4932925ac
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2024-01-10 10:48:51 -0500

    work: vector field animation

--- animations/homepage.py
+++ animations/homepage.py
@@ -5,6 +5,9 @@ import numpy as np;
 
 class MainScene(Scene):
     def construct(self):
+        self.vectorField()
+
+    def plane(self):
         # > - Crear grid del plano - <
         planeGrid = NumberPlane(
             background_line_style = {
@@ -43,21 +46,59 @@ class MainScene(Scene):
         self.vectorSpace()
 
     def vectorSpace(self):
-        tracker = ValueTracker(1)
-        
-        vectorField = always_redraw(
+        funcs = [
+            lambda pos: np.array([pos[1], pos[0], 0 ]),
+            lambda pos: np.array([np.sin(pos[0] / 2) - np.cos(pos[1] / 2), np.sin(pos[0] / 2), 0 ]),
+            lambda pos: np.array([1, np.sin(pos[0]*pos[0] + pos[1]*pos[1]), 0 ]),
+            lambda pos: np.array([pos[1] - np.cos((pos[0]*pos[0] + pos[1]*pos[1]) ** 0.5), np.cos((pos[0]*pos[0] + pos[1]*pos[1]) ** 0.5), 0 ]),
+        ]
+        initFunc = funcs[-1]
+
+        getColor = lambda string: ManimColor.from_hex(string)
+        colors = [
+            RED_A, TEAL_C, getColor("#AF62FF"), getColor("#D2A6FF") 
+        ]
+
+        stream_lines = StreamLines(initFunc, stroke_width=2, max_anchors_per_line=30, colors = colors)
+        vector_field = ArrowVectorField(initFunc, colors = colors)
+
+        self.add(stream_lines)
+        self.add(vector_field)
+
+        stream_lines.start_animation(warm_up=True, flow_speed=1.5)
+        self.wait(3)
+
+        getColor = lambda string: ManimColor.from_hex(string)
+        v = ValueTracker(0)
+        a = always_redraw(lambda: ArrowVectorField(
+            lambda pos: np.array([pos[1] * np.sin(v.get_value()), pos[0] * np.sin(v.get_value()), 0 ]),
+            colors = [
+                TEAL_C, RED_A, getColor("#D2A6FF"), getColor("A146FF")
+            ]
+        ))
+        self.play(Create(a))
+        self.play(v.animate.set_value(3), run_time = 3)
+
+
+    def vectorField(self):
+        getColor = lambda string: ManimColor.from_hex(string)
+        colors = [
+            TEAL_C, RED_A, getColor("#BB52FF"), getColor("#B369FF"), getColor("#C184FF"), getColor("#D2A6FF")
+        ]
+        v = ValueTracker(0)
+
+        streamFunc = lambda pos: np.array([pos[1] - np.cos((pos[0]*pos[0] + pos[1]*pos[1]) ** 0.5), np.cos((pos[0]*pos[0] + pos[1]*pos[1]) ** 0.5), 0 ])
+
+        stream_lines = StreamLines(streamFunc, stroke_width=2, max_anchors_per_line=30, colors = colors)
+        vector_field = always_redraw(
             lambda: ArrowVectorField(
-                lambda pos: np.array(
-                    [
-                        pos[0] * tracker,
-                        pos[1] * tracker,
-                    ]
-                )
-                color = getColor("#D2A6FF"),
+                lambda pos: (0.5 * (np.sin(v.get_value()) + 1.5)) * np.array([pos[1] - np.cos((pos[0]*pos[0] + pos[1]*pos[1]) ** 0.5), np.cos((pos[0]*pos[0] + pos[1]*pos[1]) ** 0.5), 0 ]), 
+                colors = colors
             )
         )
 
-        self.add(vectorField)
-        self.wait()
-        self.play(tracker.animate.set_value(2))
-        self.play(tracker.animate.set_value(-1))
+        self.add(stream_lines)
+        self.play(Create(vector_field))
+
+        stream_lines.start_animation(warm_up=True, flow_speed=1.5)
+        self.play(v.animate.set_value(2 * PI), run_time = 6)
