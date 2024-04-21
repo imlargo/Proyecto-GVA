0517e586d8f4ca2161956500c6c6ac25647eb471
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2024-04-21 05:20:48 +0000

    add: Latex suport and dinamic lesson routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@ import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 
 import mdx from "@astrojs/mdx";
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), mdx()]
+    integrations: [svelte(), mdx()],
+    markdown: {
+    	remarkPlugins: [remarkMath],
+    	rehypePlugins: [rehypeKatex]
+    }
 });
--- content/lessons/lesson-9.md
+++
@@ -1,8 +0,0 @@
----
-layout: ../../layouts/BaseLesson.astro
-
-num: '2'
-title: 'Operaciones fundamentales con vectores y sus propiedades'
-description: '...'
-video: 'https://drive.google.com/file/d/1lR8yixoFN9cpzuUowdCGBnr00fyOCkv2/preview'
----
